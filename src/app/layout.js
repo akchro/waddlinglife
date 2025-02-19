2c87858fbb4cead1e505ceb52e10368a044ea829
Andy Khau <fakeemail1441@gmail.com>
2025-02-19 03:44:01 -0800

    Add umami analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 
 export const metadata = {
@@ -10,10 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body
-      >
+    <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="06ecfd2c-c86f-49b9-ba7b-ab0b2478f8d6"
+    />
+    <body
+    >
         {children}
-      </body>
+    </body>
+
     </html>
   );
 }
